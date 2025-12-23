7eab531d95ebe4eca91c77e7139658f2fecde5c1
wl98336 <wl98336@163.com>
2025-12-23 13:21:49 +0800

    images for dictionary and inner navigation of link inside dictionary

--- src/app/postlogin/dictionary/dictionary.ts
+++ src/app/postlogin/dictionary/dictionary.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { DictService } from '../../service/dict.service';
@@ -7,6 +13,7 @@ import { DictService } from '../../service/dict.service';
   imports: [FormsModule],
   templateUrl: './dictionary.html',
   styleUrl: './dictionary.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class Dictionary {
   value: string = '';
@@ -14,9 +21,10 @@ export class Dictionary {
   constructor(
     private dictService: DictService,
     private sanitizer: DomSanitizer,
-    private ref: ChangeDetectorRef
+    private ref: ChangeDetectorRef,
+    private el: ElementRef<HTMLElement>
   ) {
-    console.log("Dictionary constructor");
+    console.log('Dictionary constructor');
   }
 
   onKeydown(event: KeyboardEvent) {
@@ -27,15 +35,49 @@ export class Dictionary {
   lookup(event: any): void {
     this.dictService.lookupDict(this.value).subscribe({
       next: (data) => {
-        this.safeHtmlContent = this.sanitizer.bypassSecurityTrustHtml((data as any).definition);
-        const domParser = new DOMParser();
-        const dict = domParser.parseFromString((data as any).definition, 'text/html');
-        console.log('dict', dict.body.children, dict.body.childNodes);
-        this.ref.markForCheck();
+        this.safeHtmlContent = this.sanitizer.bypassSecurityTrustHtml(
+          (data as any).definition.replace(/<head>.*<\/head>/g, '').replace(/<img src="([a-zA-Z0-9_]+.jpg)">/g, "<img src=\"mdd/$1\">")
+        );
+        this.ref.detectChanges();
+        //set Time out because ngAfterViewChecked run too many times.
+        setTimeout(() => {
+          this.el.nativeElement.querySelectorAll<HTMLDivElement>('.pic_thumb').forEach((ele) => {
+            ele.onclick = null;
+            ele.addEventListener('click', this.expand_big.bind(this));
+          });
+          this.el.nativeElement.querySelectorAll<HTMLDivElement>('.big_pic').forEach((ele) => {
+            ele.onclick = null;
+            ele.addEventListener('click', this.expand_thumb.bind(this));
+          });
+          this.el.nativeElement
+            .querySelectorAll<HTMLAnchorElement>('a[href^=entry]')
+            .forEach((ele) => {
+              ele.onclick = null;
+              ele.addEventListener('click', this.entryWord.bind(this));
+            });
+        }, 30);
       },
       error: (HttpErrorResponse) => {
         console.log(HttpErrorResponse.message); // show error message, if any.
       },
     });
+  }
+
+  expand_big(event: Event): void {
+    const div = event.currentTarget as HTMLDivElement;
+    div?.setAttribute('style', 'display:none');
+    div?.parentElement?.querySelector('.big_pic')?.setAttribute('style', 'display:block');
+  }
+  expand_thumb(event: Event): void {
+    const div = event.currentTarget as HTMLDivElement;
+    div.setAttribute('style', 'display:none');
+    div.parentElement?.querySelector('.pic_thumb')?.setAttribute('style', 'display:block');
+  }
+  entryWord(event: Event) {
+    event.preventDefault();
+    const ele = event?.currentTarget as HTMLAnchorElement;
+    const entry = ele.href.split('//')[1];
+    this.value = entry;
+    this.lookup(event);
   }
 }
